7f8c64cf6cb086725e187bf42a0e5ba43456dd1c
mkubisa1 <mkubisa1@binghamton.edu>
2022-10-14 12:19:45 -0400

    implemented writing results to output

--- bimodalTwoBit.cpp
+++ bimodalTwoBit.cpp
@@ -28,40 +28,40 @@ bool bimodalTwoBit::predict(long long unsigned int inputAddr, string actualOutco
     string pcBits = getBinary( inputAddr,0 ).substr( getBinary(inputAddr,0).length()-binaryLength); //extract the last binaryLength bits from addr
     auto iteration = table.find(pcBits); //get index in map of pcBits entry
 
-                    cout << "pcBits = " << pcBits << " = table entry of " << iteration->first << endl;
+                    //cout << "pcBits = " << pcBits << " = table entry of " << iteration->first << endl;
 
     //consolidate strong and weak versions of prediction into a single predicton:
     string prediction;
     if(iteration->second == "TT" || iteration->second == "TN") {prediction = "T";}  //taken
     if(iteration->second == "NN" || iteration->second == "NT") {prediction = "NT";} //not taken (NOTE: "NT" in prediction table means "weak not taken"; actualOutcome of NT has no weak/strong affiliation.)
 
-                    cout << "based on table entry of " << iteration->second << ", overall prediction is " << prediction << endl;
-                    cout << "does actual outcome " << actualOutcome << " = " << prediction << "?" << endl;
+                    //cout << "based on table entry of " << iteration->second << ", overall prediction is " << prediction << endl;
+                    //cout << "does actual outcome " << actualOutcome << " = " << prediction << "?" << endl;
 
     //compare prediction to actualOutcome:
     if(prediction == actualOutcome) {
         //make weak predictions stronger:
 
-                    cout << "prediction correct. table entry " << iteration->second;
+                    //cout << "prediction correct. table entry " << iteration->second;
 
         if(iteration->second == "TN") {iteration->second = "TT";}
         else if(iteration->second == "NT") {iteration->second = "NN";}
 
-                    cout << " updated to " << iteration->second << endl << endl;
+                    //cout << " updated to " << iteration->second << endl << endl;
 
         return true;
     }
     else {
         //update prediction for this pcBits:
 
-                    cout << "prediction wrong. table entry " << iteration->second;
+                    //cout << "prediction wrong. table entry " << iteration->second;
 
         if(iteration->second == "NN") {iteration->second = "NT";}       //case: was strong not taken
         else if(iteration->second == "NT") {iteration->second = "TN";}  //case: was weak not taken
         else if(iteration->second == "TN") {iteration->second = "NT";}  //case: was weak taken
         else if(iteration->second == "TT") {iteration->second = "TN";}  //case: was strong not taken
 
-                    cout << " updated to " << iteration->second << endl << endl;
+                    //cout << " updated to " << iteration->second << endl << endl;
 
         return false;
     }
--- main.cpp
+++ main.cpp
@@ -36,16 +36,22 @@ int main(int argc, char *argv[]) { //to run program: ./predictors test_input.txt
 
     bimodalOneBit bob16(16), bob32(32), bob128(128), bob256(256), bob512(512), bob1024(1024), bob2048(2048);
     bimodalTwoBit btb16(16), btb32(32), btb128(128), btb256(256), btb512(512), btb1024(1024), btb2048(2048);
+    int instCount = 0; //the number of instructions/lines in infile
 
     while(getline(infile, line)) {
+
+        instCount += 1;
 
         stringstream s(line);
         s >> std::hex >> addr >> behavior >> std::hex >> target; //extract each portion of the input line and place into necessary variables
 
         //assert correctness of each method:
+        cout << "running alwaysTaken()..." << endl;
         if(alwaysTaken() == behavior) {correctCount[0] += 1;}
+        cout << "running alwaysNotTaken()..." << endl;
         if(neverTaken() == behavior) {correctCount[1] += 1;}
 
+        cout << "running bimodalOneBit()..." << endl;
         if(bob16.predict(addr, behavior))   {correctCount[2] += 1;}
         if(bob32.predict(addr, behavior))   {correctCount[3] += 1;}
         if(bob128.predict(addr, behavior))  {correctCount[4] += 1;}
@@ -54,14 +60,25 @@ int main(int argc, char *argv[]) { //to run program: ./predictors test_input.txt
         if(bob1024.predict(addr, behavior)) {correctCount[7] += 1;}
         if(bob2048.predict(addr, behavior)) {correctCount[8] += 1;}
 
-        // if(btb16.predict(addr, behavior))   {correctCount[9] += 1;}
-        // if(btb32.predict(addr, behavior))   {correctCount[10] += 1;}
-        // if(btb128.predict(addr, behavior))  {correctCount[11] += 1;}
-        // if(btb256.predict(addr, behavior))  {correctCount[12] += 1;}
-        // if(btb512.predict(addr, behavior))  {correctCount[13] += 1;}
-        // if(btb1024.predict(addr, behavior)) {correctCount[14] += 1;}
+        cout << "running bimodalTwoBit()..." << endl;
+        if(btb16.predict(addr, behavior))   {correctCount[9] += 1;}
+        if(btb32.predict(addr, behavior))   {correctCount[10] += 1;}
+        if(btb128.predict(addr, behavior))  {correctCount[11] += 1;}
+        if(btb256.predict(addr, behavior))  {correctCount[12] += 1;}
+        if(btb512.predict(addr, behavior))  {correctCount[13] += 1;}
+        if(btb1024.predict(addr, behavior)) {correctCount[14] += 1;}
         if(btb2048.predict(addr, behavior)) {correctCount[15] += 1;}
     }
+    infile.close();
+    
+    outfile << correctCount[0] << "," << instCount << ";" << endl;                          //alwaysTaken
+    outfile << correctCount[1] << "," << instCount << ";" << endl;                          //alwaysNotTaken
+    for(int i = 2; i < 9; i++) { outfile << correctCount[i] << "," << instCount << "; ";}   //bimodalOneBit
+    outfile << endl;
+    for(int i = 9; i < 16; i++) { outfile << correctCount[i] << "," << instCount << "; ";}  //bimodalTwoBit
+    outfile << endl;
+
+    outfile.close();
 
     return 0;
 }
